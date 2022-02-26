208422c60cf35f395b90a105cde88f243f15b776
Gavin Neal <grneal@bsu.edu>
2022-02-26 17:24:56 -0500

    Add makePoints function to turn normalized data to points

--- cs455_homework3_program_VaalNeal.py
+++ cs455_homework3_program_VaalNeal.py
@@ -1,17 +1,12 @@
 import csv
 import math
+from random import randint
 
 myDict = {}
 
-with open('auto-mpg.csv', mode='r') as read_obj:
-    reader = csv.reader(read_obj)
-    dataAsRows = list(reader)
-
-def makeInitialCentroid(n, data):
-    r = []
+def makeInitialCentroids(n, dataPoints):
     for x in n:
-        r.append(x[0])
-    return r
+        myDict.__setattr__(dataPoints[randint(0,99)], [])
 
 def askUserInput():
     print("Please enter the number of clusters you want to form, 2 or 3. ")
@@ -23,34 +18,34 @@ def askUserInput():
             Index = int(input("That was an invalid number, please re-enter either 2 or 3: "))
 
 
+def makePoints(data):
+    pointList = []
+    for x in range(len(data[0])):
+        pointList.append([data[0][x], data[1][x]])
+    return pointList
+
 def normalize_data(data, min, max):
     normal_column = []
     for i in range(len(data)):
-        new_value = round((data[i] - min) / (max - min), 2) * 100
+        new_value = round((data[i] - min) / (max - min) * 100)
         normal_column.append(new_value)
     return normal_column
 
-def compute_distance_numerical(new_data):
-    distance_matrix = []
-    for i in range(len(new_data)):
-        temp = []
-        for j in range(len(new_data)):
-            distance = round(math.fabs(new_data[i] - new_data[j]),2)
-            temp.append(distance)
-        distance_matrix.append(temp)
-    return distance_matrix
+with open('auto-mpg.csv', mode='r') as read_obj:
+    reader = csv.reader(read_obj)
+    dataAsRows = list(reader)
 
 if __name__ == '__main__':
+    dataPoints = []
     open('auto-mpg.csv')
 
     normalized_data = []
-    for j in range(0,1):
+    for j in range(0,2):
         columnList = []
         for i in range(1, len(dataAsRows)):
                 columnList.append(int(dataAsRows[i][j]))
         normalized_data.append(normalize_data(columnList, min(columnList), max(columnList)))
+    dataPoints = makePoints(normalized_data)
 
-    first_numerical_distance = compute_distance_numerical(normalized_data[0])
-    second_numerical_distance = compute_distance_numerical(normalized_data[1])
     
 
